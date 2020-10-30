1ffd94ae61da330bd4f5db6cc978d1bfe661b7ef
stephen shen <723809534@qq.com>
2020-10-30 14:56:24 +0800

    创建自定义注解

---
+++ src/main/java/com/example/dingding/annotation/DingDingLog.java
@@ -0,0 +1,25 @@
+package com.example.dingding.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DingDingLog {
+//    //表示操作是哪个服务（模块）
+//    String module() default "xxx服务";
+
+    /**
+     * add update delete
+     * @return
+     */
+    String type() default "add";
+
+    //操作者id
+    String user() default "1";
+
+    //操作描述
+    String operation() default "";
+}
