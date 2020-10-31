58eaf0fa6739659c95b9e29d07837c7c2269886c
stephen shen <723809534@qq.com>
2020-10-31 14:55:34 +0800

    创建BaseService

---
+++ src/main/java/com/example/dingding/commons/service/BaseService.java
@@ -0,0 +1,13 @@
+package com.example.dingding.commons.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @description
+ * @className: BaseService
+ * @package: com.example.dingding.commons.service
+ * @author: Stephen Shen
+ * @date: 2020/10/31 下午2:55
+ */
+public interface BaseService<T> extends IService<T> {
+}
