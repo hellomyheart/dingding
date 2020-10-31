afbc4e115f6620ec1c5d9791bd1175ed1168981f
stephen shen <723809534@qq.com>
2020-10-31 14:50:40 +0800

    创建BaseMapper

---
+++ src/main/java/com/example/dingding/commons/mapper/BaseMapper.java
@@ -0,0 +1,11 @@
+package com.example.dingding.commons.mapper;
+
+/**
+ * @description
+ * @className: BaseMapper
+ * @package: com.example.dingding.commons.mapper
+ * @author: Stephen Shen
+ * @date: 2020/10/31 下午2:50
+ */
+public interface BaseMapper<T> extends com.baomidou.mybatisplus.core.mapper.BaseMapper<T> {
+}
