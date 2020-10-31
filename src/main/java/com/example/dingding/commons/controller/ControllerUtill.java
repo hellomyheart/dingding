7fd486058ec5b783fd65e675d5bc4832244b9371
stephen shen <723809534@qq.com>
2020-10-31 15:03:17 +0800

    封装controller

--- src/main/java/com/example/dingding/commons/controller/ControllerUtill.java
+++ src/main/java/com/example/dingding/commons/controller/ControllerUtill.java
@@ -1,5 +1,16 @@
 package com.example.dingding.commons.controller;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.example.dingding.commons.entity.BaseEntity;
+import com.example.dingding.commons.pagehelper.PageHelperParam;
+import com.example.dingding.commons.service.BaseService;
+import com.example.dingding.vo.ResponseResult;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * @description
  * @className: ControllerUtill
@@ -8,4 +19,111 @@ package com.example.dingding.commons.controller;
  * @date: 2020/10/31 下午2:38
  */
 public class ControllerUtill {
+
+    /**
+     * 插入一条记录
+     *
+     * @param e
+     * @return
+     */
+    public <S extends BaseService<E>, E extends BaseEntity> ResponseResult save(S s, E e) {
+        boolean save = s.save(e);
+        return result(save);
+    }
+
+    /**
+     * 根据Id修改一条记录
+     *
+     * @param e
+     * @return
+     */
+    public <S extends BaseService<E>, E extends BaseEntity> ResponseResult updateById(S s, E e) {
+        boolean update = s.updateById(e);
+        return result(update);
+    }
+
+    /**
+     * 根据ID删除
+     *
+     * @param id
+     * @return
+     */
+    public <S extends BaseService<E>, E extends BaseEntity> ResponseResult removeById(S s, Serializable id) {
+        boolean removeById = s.removeById(id);
+        return result(removeById);
+    }
+
+    /**
+     * 查询
+     *
+     * @param queryWrapper
+     * @return
+     */
+    public <S extends BaseService<E>, E extends BaseEntity> ResponseResult list(S s, Wrapper<E> queryWrapper) {
+        List<E> list = s.list(queryWrapper);
+        return result(list);
+    }
+
+    /**
+     * 根据ID查询
+     *
+     * @param id
+     * @return
+     */
+    public <S extends BaseService<E>, E extends BaseEntity> ResponseResult getById(S s, Serializable id) {
+        E byId = s.getById(id);
+        return result(byId);
+    }
+
+    /**
+     * 分页查询
+     *
+     * @param queryWrapper
+     * @return
+     */
+    public <S extends BaseService<E>, E extends BaseEntity> ResponseResult pageList(S s, Wrapper<E> queryWrapper, PageHelperParam pageHelperParam) {
+
+        PageHelper.startPage(pageHelperParam.getPageNum(), pageHelperParam.getPageSize());
+        List<E> list = s.list(queryWrapper);
+        PageInfo<E> result = new PageInfo<>(list);
+        return result(result);
+    }
+
+
+    /**
+     * 根据 Wrapper，查询一条记录
+     *
+     * @param queryWrapper
+     * @return
+     */
+    public <S extends BaseService<E>, E extends BaseEntity> ResponseResult getOne(S s, Wrapper<E> queryWrapper) {
+        E one = s.getOne(queryWrapper,false);
+        return result(one);
+    }
+
+    public ResponseResult result(boolean flag) {
+        if (flag) {
+            return ResponseResult.ok();
+        }
+        return ResponseResult.fail();
+    }
+
+
+    public ResponseResult result(PageInfo pageInfo) {
+        return ResponseResult.ok(pageInfo);
+    }
+
+    public ResponseResult result(List list) {
+        return ResponseResult.ok(list);
+    }
+
+
+    public <E extends BaseEntity> ResponseResult result(E e) {
+        if (e != null) {
+            return ResponseResult.ok(e);
+        }
+        return ResponseResult.fail();
+    }
+
+
 }
