f0ca01d6a77a9522073eaf7be0cf656e8da4098e
stephen shen <723809534@qq.com>
2020-10-31 15:04:56 +0800

    注入controllerUtilsBean

--- src/main/java/com/example/dingding/DingdingApplication.java
+++ src/main/java/com/example/dingding/DingdingApplication.java
@@ -1,9 +1,11 @@
 package com.example.dingding;
 
+import com.example.dingding.commons.controller.ControllerUtill;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -14,6 +16,11 @@ public class DingdingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DingdingApplication.class, args);
+    }
+
+    @Bean
+    public ControllerUtill controllerUtils(){
+        return new ControllerUtill();
     }
 
 }
